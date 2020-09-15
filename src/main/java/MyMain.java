171a3d46b6f2a77dc058f7d5f069f8ff627a6dc3
23eliu <23eliu@athenian.org>
2020-09-15 00:36:13 +0000

    finished main and scanner code

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -3,12 +3,33 @@ import java.util.Scanner;
 public class MyMain {
     
     public static int binaryToDecimal(String binary) { 
-        return -1;
-        // REPLACE WITH YOUR CODE HERE
+        int sum = 0;
+        int bin = Integer.parseInt(binary);
+        int mult = 1;
+
+        if (binary.length() == 1) {
+            return bin;
+        }
+        else {
+            sum += bin % 10;
+            bin /= 10;
+            
+            while (bin > 0) {
+                mult *= 2;
+                sum += mult * (bin % 10);
+                bin /= 10;
+            }
+
+            return sum;
+        }
     }
     
     
     public static void main(String[] args) {
-        // YOUR CODE HERE
+        Scanner scan = new Scanner(System.in); 
+        System.out.println("Please enter a decimal number: ");
+        System.out.println(binaryToDecimal(scan.next()));
+
+        scan.close();
     }
 }
